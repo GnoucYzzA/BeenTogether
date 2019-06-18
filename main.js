f1443e17c3092697093da1d18220001f58cd36a4
Nam Cuong <cuongndngcd17192@fpt.edu.vn>
2019-06-18 22:18:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var min = document.querySelector('.min');
   var second = document.querySelector('.second');
 
-  var startDate = new Date(2019, 03, 30);
+  var startDate = new Date(2019, 04, 1);
   days.innerText = Math.floor((new Date - startDate)/86400000);
   countTime();
 
